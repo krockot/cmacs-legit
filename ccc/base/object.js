8dd54ec33989d39167d0320bcd6f002b645e8cd7
Ken Rockot <ken@oz.gs>
2014-09-27 12:56:24 -0700

    Add more to base Object

--- ccc/base/object.js
+++ ccc/base/object.js
@@ -1,6 +1,10 @@
 // The Cmacs Project.
 // Copyright forever, the universe.
 
+goog.provide('ccc.base.F');
+goog.provide('ccc.base.NIL');
+goog.provide('ccc.base.T');
+goog.provide('ccc.base.UNSPECIFIED');
 goog.provide('ccc.base.Object');
 
 
@@ -14,3 +18,72 @@ goog.provide('ccc.base.Object');
 ccc.base.Object = function() {
 };
 
+
+/**
+ * Default strict equality implementation: native object identity.
+ *
+ * @param {!ccc.base.Object} other
+ * @return {boolean}
+ */
+ccc.base.Object.prototype.eq = function(other) {
+  return this === other;
+};
+
+
+/**
+ * Default equivalence implementation: fallback to strict equality.
+ *
+ * @param {!ccc.base.Object} other
+ * @return {boolean}
+ */
+ccc.base.Object.prototype.eqv = function(other) {
+  return this.eq(other);
+};
+
+
+/**
+ * Default non-strict equality implementation: fallback to equivalence.
+ *
+ * @param {!ccc.base.Other} other
+ * @return {boolean}
+ */
+ccc.base.Object.prototype.equal = function(other) {
+  return this.eqv(other);
+};
+
+
+
+/**
+ * The global NIL object.
+ *
+ * @public {!ccc.base.Object}
+ * @const
+ */
+ccc.base.NIL = new ccc.base.Object();
+
+
+/**
+ * The global UNSPECIFIED object.
+ *
+ * @public {!ccc.base.Object}
+ * @const
+ */
+ccc.base.UNSPECIFIED = new ccc.base.Object();
+
+
+/**
+ * The global T (#t) object.
+ *
+ * @public {!ccc.base.Object}
+ * @const
+ */
+ccc.base.T = new ccc.base.Object();
+
+
+/**
+ * The global F (#f) object.
+ *
+ * @public {!ccc.base.Object}
+ * @const
+ */
+ccc.base.F = new ccc.base.Object();

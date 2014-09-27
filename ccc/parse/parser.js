e6bf0458bb9bfee25bc4e08f75820ecff75dcfa9
Ken Rockot <ken@oz.gs>
2014-09-27 12:58:11 -0700

    Fill out basic Parser logic and test framework

--- ccc/parse/parser.js
+++ ccc/parse/parser.js
@@ -3,11 +3,35 @@
 
 goog.provide('ccc.parse.Parser');
 
+goog.require('ccc.base.NIL');
 goog.require('ccc.base.Object');
 goog.require('ccc.parse.ObjectReader');
 goog.require('ccc.parse.Token');
 goog.require('ccc.parse.TokenReader');
 goog.require('ccc.parse.TokenType');
+
+
+
+/**
+ * The result of a single transition step.
+ *
+ * @typedef {{
+ *   object: (!ccc.base.Object|undefined),
+ *   state: (!Transition_|undefined)
+ * }}
+ * @private
+ */
+var TransitionResult_;
+
+
+
+/**
+ * A state transition function.
+ *
+ * @typedef {function(!ccc.parse.Token):!TransitionResult_}
+ * @private
+ */
+var Transition_;
 
 
 
@@ -24,17 +48,41 @@ goog.require('ccc.parse.TokenType');
 ccc.parse.Parser = function(tokenReader) {
   /** @private {!ccc.parse.TokenReader} */
   this.tokenReader_ = tokenReader;
+
+  /** @private {!ccc.parse.ParserState} */
+  this.state_ = START_STATE_;
 };
 
 
 /** @override */
 ccc.parse.Parser.prototype.readObject = function() {
-  var object
-  while (true) {
-    var token = this.tokenReader_.readToken();
-    if (goog.isNull(token)) {
-      return null;
+  return this.tokenReader_.readToken().then(function(token) {
+    var result = this.state_(token);
+    if (goog.isDef(result.object)) {
+      this.state_ = START_STATE_;
+      return result.object;
     }
-  }
-  return null;
+    goog.asserts.assert(goog.isDef(result.state), 'Invalid state transition.');
+    this.state_ = result.state;
+    return this.readObject();
+  }, null, this);
 };
+
+
+/**
+ * The parsing state machine.
+ *
+ * @private {!Transition_}
+ * @const
+ */
+var START_STATE_ = (function() {
+  /**
+   * @param {!ccc.parseToken} token
+   * @return {!TransitionResult_}
+   */
+  return function(token) {
+    if (goog.isNull(token))
+      return { object: null };
+    return { object: ccc.base.NIL };
+  };
+}());
---
+++ ccc/parse/parser_test.js
@@ -0,0 +1,155 @@
+// The Cmacs Project.
+// Copyright forever, the universe.
+
+goog.provide('ccc.parse.ParserTest');
+goog.setTestOnly('ccc.parse.ParserTest');
+
+goog.require('ccc.base.NIL');
+goog.require('ccc.base.Object');
+goog.require('ccc.parse.Parser');
+goog.require('ccc.parse.Token');
+goog.require('ccc.parse.TokenReader');
+goog.require('ccc.parse.TokenType');
+goog.require('goog.testing.AsyncTestCase');
+goog.require('goog.testing.jsunit');
+
+
+var K = ccc.parse.TokenType;
+var asyncTestCase = goog.testing.AsyncTestCase.createAndInstall(document.title);
+
+function setUpPage() {
+  asyncTestCase.stepTimeout = 200;
+}
+
+function continueTesting() {
+  asyncTestCase.continueTesting();
+}
+
+function justFail(reason) {
+  console.error(reason);
+  fail(reason);
+}
+
+// Simplified token constructors for test setup.
+
+var OPEN_LIST = function(bracket) {
+  return new ccc.parse.Token(K.OPEN_LIST, bracket, 1, 1);
+};
+var CLOSE_LIST = function(bracket) {
+  return new ccc.parse.Token(K.CLOSE_LIST, bracket, 1, 1);
+};
+var QUOTE = function() { return new ccc.parse.Token(K.QUOTE, '\'', 1, 1); };
+var UNQUOTE = function() { return new ccc.parse.Token(K.UNQUOTE, ',', 1, 1); };
+var UNQUOTE_SPLICING = function() {
+  return new ccc.parse.Token(K.UNQUOTE_SPLICING, ',@', 1, 1);
+};
+var QUASIQUOTE = function() {
+  return new ccc.parse.Token(K.QUASIQUOTE, '`', 1, 1);
+};
+var OMIT_DATUM = function() {
+  return new ccc.parse.Token(K.OMIT_DATUM, '#;', 1, 1);
+};
+var TRUE = function() { return new ccc.parse.Token(K.TRUE, '#t', 1, 1); };
+var FALSE = function() { return new ccc.parse.Token(K.FALSE, '#f', 1, 1); };
+var UNSPECIFIED = function() {
+  return new ccc.parse.Token(K.UNSPECIFIED, '#?', 1, 1);
+};
+var OPEN_VECTOR = function(bracket) {
+  return new ccc.parse.Token(K.OPEN_VECTOR, '#' + bracket, 1, 1);
+};
+var SYMBOL = function(name) {
+  return new ccc.parse.Token(K.SYMBOL, name, 1, 1);
+};
+var CHAR_LITERAL = function(value) {
+  return new ccc.parse.Token(K.CHAR_LITERAL,
+      '#\\' + String.fromCharCode(value), 1, 1);
+};
+var STRING_LITERAL = function(value) {
+  return new ccc.parse.Token(K.STRING_LITERAL, '"' + value + '"', 1, 1);
+};
+var NUMERIC_LITERAL = function(value) {
+  return new ccc.parse.Token(K.NUMERIC_LITERAL, value.toString(), 1, 1);
+};
+var DOT = function() { return new ccc.parse.Token(K.DOT, '.', 1, 1); };
+
+// Some additional shortcuts
+
+var NIL = ccc.base.NIL;
+var UNSPECIFIED = ccc.base.UNSPECIFIED;
+var T = ccc.base.T;
+var F = ccc.base.F;
+
+// Test framework for the parser tests
+
+/** @implements {ccc.parse.TokenReader} */
+var TestTokenReader = function(tokens) {
+  this.tokens_ = tokens;
+};
+
+/** @override */
+TestTokenReader.prototype.readToken = function() {
+  if (this.tokens_.length == 0)
+    return goog.Promise.resolve(null);
+  return goog.Promise.resolve(this.tokens_.shift());
+};
+
+// Expect a specific object to match under equality (Object.equal).
+var E = function(match) {
+  return function(parser) {
+    return parser.readObject().then(function(object) {
+      assertNotNull('Ran out of parsed objects!', object);
+      assert('Object mismatch', object.equal(match));
+      return true;
+    });
+  };
+};
+
+// Expect failure on readObject.
+var F = function(parser) {
+  return new goog.Promise(function(resolve, reject) {
+    parser.readObject().then(
+        reject.bind(null, 'Expected failure, got success.'),
+        resolve.bind(null, false));
+  });
+};
+
+// Asynchronous test which takes a token list and a set of top-level object
+// expectations.
+var P = function(tokens, expectations) {
+  var reader = new TestTokenReader(tokens);
+  var parser = new ccc.parse.Parser(reader);
+  asyncTestCase.waitForAsync();
+  new goog.Promise(function(resolve, reject) {
+    var checkExpectations = function(expectations) {
+      if (expectations.length == 0) {
+        parser.readObject().then(function(object) {
+          assertNull(object);
+          resolve(null);
+        }, reject);
+      } else {
+        var expectation = expectations.shift();
+        expectation(parser).then(function(keepChecking) {
+          if (keepChecking)
+            checkExpectations(expectations);
+          else
+            resolve(null);
+        }, reject);
+      }
+    };
+    checkExpectations(expectations);
+  }).then(continueTesting, justFail);
+};
+
+// Tests below this line
+
+function testDumbParser() {
+  P([
+    K.OPEN_LIST,
+    K.SYMBOL,
+    K.CLOSE_FORM
+  ], [
+    E(NIL),
+    E(NIL),
+    E(NIL)
+  ]);
+}

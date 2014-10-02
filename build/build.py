bc309e3ee4537c731f35e8104eee8fc75b9840a6
Ken Rockot <ken@oz.gs>
2014-10-01 23:28:55 -0700

    Simplify build and test. No more large tree copies.

--- build/build.py
+++ build/build.py
@@ -2,17 +2,12 @@
 
 
 import inspect
-import itertools
 import json
 import os
-import posixpath
-import re
 import shutil
 import subprocess
 import sys
 import tempfile
-import urllib
-import urlparse
 import zipfile
 
 
@@ -40,41 +35,11 @@ def _PrintError(*args):
   sys.stderr.flush()
 
 
-def _InitializeOutput(app_path, test_path, out_path):
+def _InitializeOutput(app_path, out_path):
   if os.path.isdir(out_path):
     shutil.rmtree(out_path)
   app_out_path = os.path.join(out_path, 'app')
-  test_out_path = os.path.join(out_path, 'test')
   shutil.copytree(app_path, app_out_path)
-  os.mkdir(test_out_path)
-  shutil.copy(os.path.join(test_path, 'index.html'),
-              os.path.join(test_out_path, 'index.html'));
-
-
-def _FindTarget(predicate):
-  subroot = os.getcwd()
-  while os.path.dirname(subroot) != os.path.dirname(os.path.dirname(subroot)):
-    subroot = os.path.dirname(subroot)
-  for root, dirs, files in os.walk(subroot):
-    for path in itertools.chain(dirs, files):
-      full_path = os.path.join(root, path)
-      if predicate(full_path):
-        return full_path
-  return None
-
-
-def _FindClosureLibrary():
-  def is_library_root(path):
-    return (os.path.isdir(path) and
-            os.path.isdir(os.path.join(path, 'closure')) and
-            os.path.isdir(os.path.join(path, 'closure', 'goog')))
-  return _FindTarget(is_library_root)
-
-
-def _FindClosureCompiler():
-  def is_compiler_jar(path):
-    return os.path.isfile(path) and os.path.basename(path) == 'compiler.jar'
-  return _FindTarget(is_compiler_jar)
 
 
 def _CompileJs(closure_library_root,
@@ -124,14 +89,12 @@ def _CompileJs(closure_library_root,
         ['!%s' % os.path.join(path, '**_test.js') for path in src_paths])
 
 
-def _BuildJsOutputs(src_paths, externs, out_path, debug, closure_library_root):
-  closure_compiler_jar = (os.environ.get('CLOSURE_COMPILER_JAR') or
-                          _FindClosureCompiler())
-  if closure_compiler_jar is None:
-    _PrintError('Unable to locate compiler.jar.')
-    sys.exit(1)
-  print 'Using closure-library at %s' % closure_library_root
-  print 'Using compiler.jar at %s' % closure_compiler_jar
+def _BuildJsOutputs(closure_library_root,
+                    closure_compiler_jar,
+                    src_paths,
+                    externs,
+                    out_path,
+                    debug):
   for namespace, filename in _COMPILE_TARGETS:
     output_file = os.path.join(out_path, 'app', filename)
     if not _CompileJs(closure_library_root,
@@ -144,73 +107,14 @@ def _BuildJsOutputs(src_paths, externs, out_path, debug, closure_library_root):
       sys.exit(1)
 
 
-def _BuildTestSuite(src_paths, out_path, closure_library_root):
-  closure_copy = os.path.join(out_path, 'closure')
-
-  def ignore_dot_files(d, files):
-    return [f for f in files if f.startswith('.')]
-
-  def purify_path(path, components=None):
-    if components is None:
-      components = []
-    parent, leaf = os.path.split(path)
-    components.insert(0, leaf)
-    if parent == '':
-      return posixpath.join(*components)
-    return purify_path(parent, components)
-
-  shutil.copytree(os.path.join(closure_library_root, 'closure'),
-                  closure_copy,
-                  ignore=ignore_dot_files)
+def _CalcDeps(src_paths, closure_library_root, out_path):
   print 'Calculating test deps...'
-  deps_roots = [closure_copy]
-  for path in src_paths:
-    src_copy = os.path.join(out_path, os.path.basename(path))
-    shutil.copytree(path, src_copy)
-    deps_roots.extend(['-d', src_copy])
+  deps_roots = [closure_library_root]
   subprocess.call([
       'python',
       os.path.join(closure_library_root, 'closure', 'bin', 'calcdeps.py'),
       '--output_file', os.path.join(out_path, 'deps.js'),
       '-o', 'deps'] + deps_roots)
-  template = ('<!doctype html><html><head>' +
-      '<title>Cmacs Test Suite: %s</title><head>' +
-      '<script src="/closure/goog/base.js"></script>' +
-      '<script src="/deps.js"></script>' +
-      '<script>goog.require(\'%s\');</script></head><body></body></html>')
-  print 'Setting up test server resources...'
-  with open(os.path.join(out_path, 'alltests.js'), 'w') as testList:
-    testList.write('_allTests = [')
-    for path in src_paths:
-      for root, dirs, files in os.walk(path):
-        test_files = [file for file in
-                      filter(lambda f: f.endswith('_test.js'), files)]
-        for filename in test_files:
-          with open(os.path.join(root, filename)) as test_script:
-            provide = re.search(
-                'goog\\.provide\\([\'"]([^\'"]+)[\'"]\\);',
-                test_script.read())
-            if not provide.groups():
-              print 'WARNING: No namespace provided by %s' % filename
-              test_namespace = 'please.fixme'
-            else:
-              test_namespace = provide.group(1)
-          base_path = os.path.commonprefix([root, out_path])
-          rel_path = root[len(base_path):]
-          out_dir = os.path.join(out_path, rel_path)
-          suite_name, _ = os.path.splitext(filename)
-          if not os.path.isdir(out_dir):
-            os.makedirs(out_dir)
-          html_filename = '%s.html' % suite_name
-          if os.path.isfile(os.path.join(root, html_filename)):
-            os.copy(os.path.join(root, html_filename),
-                    os.path.join(out_dir, html_filename))
-          else:
-            with open(os.path.join(out_dir, html_filename), 'w') as html_file:
-              html_file.write(template % (suite_name, test_namespace))
-          testList.write('"%s",' %
-                         purify_path(os.path.join(rel_path, html_filename)))
-    testList.write('];\n')
 
 
 def _UpdateManifest(out_path, version):
@@ -245,22 +149,19 @@ def _BuildCmacs(version, debug):
       inspect.currentframe())))
   root_path = os.path.dirname(build_script_path)
   app_path = os.path.join(root_path, 'app')
-  test_path = os.path.join(root_path, 'test')
   src_paths = map(lambda path : os.path.join(root_path, path),
                   _SOURCE_PATHS)
   externs = [os.path.join(root_path, 'externs', path) for path in _EXTERNS]
   out_path = os.path.join(root_path, 'out')
 
-  closure_library_root = (os.environ.get('CLOSURE_LIBRARY_ROOT') or
-                          _FindClosureLibrary())
-  if closure_library_root is None:
-    _PrintError('Unable to locate closure-library root.')
-    sys.exit(1)
-
-  _InitializeOutput(app_path, test_path, out_path)
-  _BuildTestSuite(src_paths, os.path.join(out_path, 'test'),
-                  closure_library_root)
-  _BuildJsOutputs(src_paths, externs, out_path, debug, closure_library_root)
+  closure_library_root = os.path.join(root_path, 'third_party',
+      'closure-library')
+  closure_compiler_jar = os.path.join(root_path, 'third_party',
+      'closure-compiler', 'compiler.jar')
+  _InitializeOutput(app_path, out_path)
+  _CalcDeps(src_paths, closure_library_root, out_path)
+  _BuildJsOutputs(closure_library_root, closure_compiler_jar, src_paths,
+      externs, out_path, debug)
   if not debug:
     print 'Packaging ZIP file with version %s' % version
     _UpdateManifest(out_path, version)
--- test/test_server.py
+++ test/test_server.py
@@ -5,27 +5,50 @@ import BaseHTTPServer
 import inspect
 import mimetypes
 import os
+import posixpath
+import re
 from urlparse import urlparse
+
+
+def purify_path(path, components=None):
+  """Converts the path specific to the current platform into a posixpath."""
+  if components is None:
+    components = []
+  parent, leaf = os.path.split(path)
+  components.insert(0, leaf)
+  if parent == '':
+    return posixpath.join(*components)
+  return purify_path(parent, components)
 
 
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
   def do_GET(self):
     if self.path == '/':
-      self.path = '/index.html'
+      self.path = '/test/index.html'
       return self.do_GET()
     self.path = urlparse(self.path).path
-    filename = os.path.basename(self.path)
-    _, ext = os.path.splitext(filename)
+
     test_script_path = os.path.dirname(os.path.abspath(inspect.getfile(
         inspect.currentframe())))
     root_path = os.path.dirname(test_script_path)
     out_path = os.path.join(root_path, 'out')
+
+    filename = os.path.basename(self.path)
+    _, ext = os.path.splitext(filename)
+
+    if self.path == '/alltests.js':
+      return self.get_all_tests(root_path)
+
+    if self.path.endswith('_test'):
+      test_suite = os.path.join(root_path, self.path[1:])
+      if os.path.isfile(test_suite + '.js'):
+        return self.get_test_suite(test_suite)
+
+
     # First try to locate the file in the project root, then fall back
     # on the output path.
     try:
       file_path = os.path.join(root_path, self.path[1:])
-      if not os.path.isfile(file_path):
-        file_path = os.path.join(out_path, 'test', self.path[1:])
       with open(file_path) as file:
         self.send_response(200)
         self.send_header('Content-type', mimetypes.types_map[ext])
@@ -33,6 +56,41 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.wfile.write(file.read())
     except:
       self.send_response(404)
+
+  def get_all_tests(self, root_path):
+    self.send_response(200)
+    self.send_header('Content-type', 'text/javascript')
+    self.end_headers()
+    self.wfile.write('_allTests=[')
+    root_dirs = os.listdir(root_path)
+    blacklist = ('third_party',)
+    for root_dir in [d for d in root_dirs
+                     if os.path.isdir(d) and d not in blacklist]:
+      for root, dirs, files in os.walk(root_dir):
+        for filename in [f for f in files if f.endswith('_test.js')]:
+          suite_name, _ = os.path.splitext(filename)
+          print os.path.join(root, suite_name)
+          suite_path = purify_path(os.path.join(root, suite_name))
+          self.wfile.write('"%s",' % suite_path)
+    self.wfile.write(']\n')
+
+
+  def get_test_suite(self, suite):
+    with open(suite + '.js') as script:
+      provide = re.search('goog\\.provide\\([\'"]([^\'"]+)[\'"]\\)',
+                          script.read())
+      if provide is None or not provide.groups():
+        self.send_response(404)
+        return
+      namespace = provide.groups(1)[0]
+    self.send_response(200)
+    self.send_header('Content-type', 'text/html')
+    self.end_headers()
+    self.wfile.write(('<!doctype html><html><head>' +
+        '<title>Cmacs Test Suite [%s]</title><script ' +
+        'src="/third_party/closure-library/closure/goog/base.js"></script>' +
+        '<script src="/out/deps.js"></script><script>goog.require("%s");' +
+        '</script></head><body></body></html>') % (suite, namespace))
 
 if __name__ == '__main__':
   mimetypes.init()

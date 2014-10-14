803b48a6b2a943c33edb7cdc4c1832fcffbfebc6
Ken Rockot <ken@oz.gs>
2014-10-14 16:37:31 -0700

    Break Match and Capture out of pattern.js

---
+++ ccc/syntax/rules/capture.js
@@ -0,0 +1,100 @@
+// The Cmacs Project.
+
+goog.provide('ccc.syntax.Capture');
+goog.provide('ccc.syntax.CaptureSet');
+
+goog.require('ccc.base');
+goog.require('goog.array');
+goog.require('goog.asserts');
+
+
+
+/**
+ * A Capture holds information about a single pattern variable captured during
+ * a syntax pattern matching operation. Captures may represent single values or
+ * nested value series with arbitrary depth.
+ *
+ * @param {!ccc.base.Object|!Array.<!ccc.syntax.Capture>} contents The contents
+ *     of this new capture. May either be a single base Object or a collection
+ *     of equal-rank captures.
+ * @constructor
+ * @public
+ */
+ccc.syntax.Capture = function(contents) {
+  /** @private {number} */
+  this.rank_ = 0;
+
+  /** @private {!ccc.base.Object|!Array.<!ccc.syntax.Capture>} */
+  this.contents_ = contents;
+
+  if (contents instanceof Array) {
+    if (contents.length > 0) {
+      var baseRank = contents[0].rank_;
+      goog.array.forEach(contents, function(capture) {
+        goog.asserts.assert(capture.rank_ == baseRank);
+      });
+      this.rank_ = baseRank + 1;
+    } else {
+      this.rank_ = 1;
+    }
+  } else {
+    goog.asserts.assert(this.rank_ == 0);
+  }
+};
+
+
+/**
+ * Returns the rank of this capture.
+ *
+ * @return {number}
+ * @public
+ */
+ccc.syntax.Capture.prototype.rank = function() {
+  return this.rank_;
+};
+
+
+/**
+ * Indicates if another Capture is equivalent to this one.
+ *
+ * @param {!ccc.syntax.Capture} other
+ * @return {boolean}
+ */
+ccc.syntax.Capture.prototype.equal = function(other) {
+  if (this.rank_ !== other.rank_)
+    return false;
+  if (!(this.contents_ instanceof Array) && !(other.contents_ instanceof Array))
+    return this.contents_.equal(other.contents_);
+  if (this.contents_.length !== other.contents_.length)
+    return false;
+  goog.asserts.assert(this.contents_ instanceof Array);
+  return goog.array.every(this.contents_, function(capture, i) {
+    return capture.equal(other.contents_[i]);
+  });
+};
+
+
+/**
+ * Stringification for test and debug output.
+ *
+ * @return {string}
+ */
+ccc.syntax.Capture.prototype.toString = function() {
+  return ((this.contents_ instanceof Array)
+      ? ('<' + this.contents_.join(', ') + '>')
+      : this.contents_.toString());
+};
+
+
+
+/**
+ * A CaptureSet is a map from symbol name to {@code ccc.syntax.Capture} objects.
+ * This is the output of a successful match operation.
+ *
+ * @typedef {!Object.<string, !ccc.syntax.Capture>}
+ * @public
+ */
+ccc.syntax.CaptureSet;
+
+
+
---
+++ ccc/syntax/rules/match.js
@@ -0,0 +1,92 @@
+// The Cmacs Project.
+
+goog.provide('ccc.syntax.Match');
+
+goog.require('ccc.syntax.Capture');
+goog.require('ccc.syntax.CaptureSet');
+goog.require('goog.asserts');
+goog.require('goog.object');
+
+
+
+/**
+ * A Match is the result of a single pattern matching attempt on an input form.
+ *
+ * @param {boolean} success
+ * @param {!ccc.syntax.CaptureSet=} opt_captures
+ * @constructor
+ * @struct
+ * @public
+ */
+ccc.syntax.Match = function(success, opt_captures) {
+  /**
+   * Indicates whether or not the match operation was successful.
+   * @public {boolean}
+   */
+  this.success = success;
+
+  /**
+   * If the match operation was successful, this contains the complete set of
+   * data necessary to expand a corresponding, well-formed template.
+   *
+   * @public {!ccc.syntax.CaptureSet}
+   */
+  this.captures = goog.isDef(opt_captures) ? opt_captures : {};
+};
+
+
+/**
+ * Merges a capture set into this match's own. There should be no overlapping
+ * capture names between the two sets.
+ *
+ * @param {!ccc.syntax.CaptureSet} captures
+ * @public
+ */
+ccc.syntax.Match.prototype.mergeCaptures = function(captures) {
+  var newKeys = goog.object.getKeys(captures);
+  for (var i = 0; i < newKeys.length; ++i) {
+    var key = newKeys[i];
+    if (goog.object.containsKey(this.captures, key))
+      throw new Error('Duplicate pattern variable name: ' + key);
+    this.captures[key] = captures[key];
+  }
+};
+
+
+/**
+ * Joins a list of matches together. Every match in the list should have an
+ * identical set of capture variables. The output Match combines the captures
+ * for each variable into a new higher-rank capture. For example, if the input
+ * set is:
+ *
+ * matches: [
+ *   { 'a': Capture(NIL, 0), 'b': Capture(#t, 0) },
+ *   { 'a': Capture(42, 0), 'b': Capture(#f, 0) }
+ * ]
+ *
+ * Then the joined output Match will be:
+ *
+ * {
+ *   'a': Capture([Capture(NIL, 0), Capture(42, 0)], 1)
+ *   'b': Capture([Capture(#t, 0), Capture(#f, 0)], 1)
+ * }
+ *
+ * @param {!Array.<!ccc.syntax.Match>} matches
+ * @return {!ccc.syntax.Match}
+ * @public
+ */
+ccc.syntax.Match.joinMatches = function(matches) {
+  goog.asserts.assert(matches.length > 0);
+  var newMatch = new ccc.syntax.Match(true);
+  goog.object.forEach(matches[0].captures, function(capture, name) {
+    newMatch.captures[name] = new ccc.syntax.Capture(goog.array.map(matches,
+        function(match) {
+          goog.asserts.assert(goog.object.containsKey(match.captures, name));
+          return match.captures[name];
+        }));
+  });
+  return newMatch;
+};
+
+
+

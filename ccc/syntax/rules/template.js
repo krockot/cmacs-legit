97aa59707be5b17d528fe2f309ed88945051454e
Ken Rockot <ken@oz.gs>
2014-10-14 23:09:32 -0700

    Fill out some basic template expansion logic

--- ccc/syntax/rules/template.js
+++ ccc/syntax/rules/template.js
@@ -5,6 +5,7 @@ goog.provide('ccc.syntax.Template');
 goog.require('ccc.base');
 goog.require('ccc.syntax.Capture');
 goog.require('ccc.syntax.CaptureSet');
+goog.require('goog.object');
 
 
 
@@ -29,7 +30,94 @@ ccc.syntax.Template = function(form) {
  *
  * @param {!ccc.syntax.CaptureSet} captures
  * @return {ccc.base.Object}
+ * @public
  */
 ccc.syntax.Template.prototype.expand = function(captures) {
-  return ccc.base.NIL;
+  return this.expandForm_(this.form_, captures);
+};
+
+
+/**
+ * Expand a template subform over a set of captures.
+ *
+ * @param {!ccc.base.Object} template
+ * @param {!ccc.syntax.CaptureSet} captures
+ * @return {ccc.base.Object}
+ * @private
+ */
+ccc.syntax.Template.prototype.expandForm_ = function(template, captures) {
+  if (template.isSymbol())
+    return this.expandSymbol_(/** @type {!ccc.base.Symbol} */ (template),
+        captures);
+  if (template.isPair())
+    return this.expandList_(template, captures);
+  if (template.isVector())
+    return this.expandVector_(/** @type {!ccc.base.Vector} */ (template),
+        captures);
+  // Anything else just expands to itself.
+  return template;
+};
+
+
+/**
+ * Expand a symbol template.
+ *
+ * @param {!ccc.base.Symbol} symbol
+ * @param {!ccc.syntax.CaptureSet} captures
+ * @return {ccc.base.Object}
+ * @private
+ */
+ccc.syntax.Template.prototype.expandSymbol_ = function(symbol, captures) {
+  // TODO: Support non-zero rank captures.
+  var capture = goog.object.get(captures, symbol.name());
+  // Non-captured symbols expand to themselves.
+  if (!goog.isDef(capture))
+    return symbol;
+  return capture.contents();
+};
+
+
+/**
+ * Expand a list template.
+ *
+ * @param {!ccc.base.Object} list
+ * @param {!ccc.syntax.CaptureSet} captures
+ * @return {ccc.base.Object}
+ * @private
+ */
+ccc.syntax.Template.prototype.expandList_ = function(list, captures) {
+  var outputElements = [];
+  while (list.isPair()) {
+    var outputElement = this.expandForm_(list.car(), captures);
+    if (goog.isNull(outputElement))
+      return null;
+    outputElements.push(outputElement);
+    list = list.cdr();
+  }
+  var tail = this.expandForm_(list, captures);
+  if (goog.isNull(tail))
+    return null;
+  return ccc.base.Pair.makeList(outputElements, tail);
+};
+
+
+/**
+ * Expand a vector template.
+ *
+ * @param {!ccc.base.Vector} vector
+ * @param {!ccc.syntax.CaptureSet} captures
+ * @return {ccc.base.Object}
+ * @private
+ */
+ccc.syntax.Template.prototype.expandVector_ = function(vector, captures) {
+  var outputElements = [];
+  for (var i = 0; i < vector.size(); ++i) {
+    var element = vector.get(i);
+    goog.asserts.assert(!goog.isNull(element));
+    var outputElement = this.expandForm_(element, captures);
+    if (goog.isNull(outputElement))
+      return null;
+    outputElements.push(outputElement);
+  }
+  return new ccc.base.Vector(outputElements);
 };
---
+++ ccc/syntax/rules/template_test.js
@@ -0,0 +1,60 @@
+// The Cmacs Project.
+
+goog.provide('ccc.syntax.TemplateTest');
+goog.setTestOnly('ccc.syntax.TemplateTest');
+
+goog.require('ccc.base');
+goog.require('ccc.syntax.Capture')
+goog.require('ccc.syntax.Template');
+goog.require('goog.testing.jsunit');
+
+var List = ccc.base.Pair.makeList;
+var Vec = function(contents) { return new ccc.base.Vector(contents); };
+var Str = function(value) { return new ccc.base.String(value); };
+var Sym = function(name) { return new ccc.base.Symbol(name); };
+var Num = function(value) { return new ccc.base.Number(value); };
+var NIL = ccc.base.NIL;
+var TRUE = ccc.base.T;
+var FALSE = ccc.base.F;
+var UNSPEC = ccc.base.UNSPECIFIED;
+
+function C(contents) {
+  return new ccc.syntax.Capture(contents);
+}
+
+function T(templateForm, captures, opt_expectedOutput) {
+  var template = new ccc.syntax.Template(templateForm);
+  var output = template.expand(captures);
+  if (goog.isDef(opt_expectedOutput) && !output.equal(opt_expectedOutput))
+    fail('Template expansion mismatch.\n' +
+         'Expected: ' + opt_expectedOutput.toString() +
+         '\nActual: ' + output.toString() + '\n');
+}
+
+function testSimpleTemplates() {
+  T(TRUE, {}, TRUE);
+  T(List([TRUE, FALSE, Str("Hello, world!")]), {},
+    List([TRUE, FALSE, Str("Hello, world!")]));
+  T(NIL, {}, NIL);
+  T(Num(42), {}, Num(42));
+  T(Vec([Num(1), Num(2), Num(3)]), {}, Vec([Num(1), Num(2), Num(3)]));
+}
+
+function testRank0Expansion() {
+  T(Sym('a'),
+    {
+      'a': C(Num(42))
+    },
+    Num(42));
+  T(List([Sym('a'), Sym('b')]),
+    {
+      'a': C(Num(42))
+    },
+    List([Num(42), Sym('b')]));
+  T(List([Sym('a')], Sym('b')),
+    {
+      'a': C(List([Num(1), Num(2)])),
+      'b': C(List([Num(3), Num(4)]))
+    },
+    List([List([Num(1), Num(2)]), Num(3), Num(4)]));
+}

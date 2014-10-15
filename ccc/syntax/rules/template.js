894ee12210ac779976851c9225285a20f496e9a6
Ken Rockot <ken@oz.gs>
2014-10-14 17:01:30 -0700

    begin le templates

--- ccc/syntax/rules/template.js
+++ ccc/syntax/rules/template.js
@@ -1,14 +1,17 @@
-// The Cmacs Project.
+  // The Cmacs Project.
 
 goog.provide('ccc.syntax.Template');
 
 goog.require('ccc.base');
+goog.require('ccc.syntax.Capture');
+goog.require('ccc.syntax.CaptureSet');
 
 
 
 /**
- * Syntax template which can be used to stamp out new forms when given data from
- * a corresponding {@code ccc.syntax.Pattern} match.
+ * Syntax template which can be used to stamp out new forms when given the
+ * {@code ccc.syntax.CaptureSet} of a corresponding {@code ccc.syntax.Pattern}
+ * match.
  *
  * @param {!ccc.base.Object} form The template form.
  * @constructor
@@ -17,4 +20,16 @@ goog.require('ccc.base');
 ccc.syntax.Template = function(form) {
   /** @private {!ccc.base.Object} */
   this.form_ = form;
+};
+
+
+/**
+ * Fully expands this template given a {@code ccc.syntax.CaptureSet}. Returns
+ * {@code null} if expansion fails.
+ *
+ * @param {!ccc.syntax.CaptureSet} captures
+ * @return {ccc.base.Object}
+ */
+ccc.syntax.Template.prototype.expand = function(captures) {
+  return ccc.base.NIL;
 };

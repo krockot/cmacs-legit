c1e211a2e43caffda86964f2d2f045efe47172da
Ken Rockot <ken@oz.gs>
2014-10-19 22:36:07 -0700

    Clean up template tests. A lot.

--- ccc/syntax/rules/template_test.js
+++ ccc/syntax/rules/template_test.js
@@ -8,32 +8,26 @@ goog.require('ccc.syntax.Capture')
 goog.require('ccc.syntax.Template');
 goog.require('goog.testing.jsunit');
 
-var List = ccc.base.Pair.makeList;
-var Vec = function(contents) { return new ccc.base.Vector(contents); };
-var Str = function(value) { return new ccc.base.String(value); };
-var Sym = function(name) { return new ccc.base.Symbol(name); };
-var Num = function(value) { return new ccc.base.Number(value); };
-var NIL = ccc.base.NIL;
-var TRUE = ccc.base.T;
-var FALSE = ccc.base.F;
-var UNSPEC = ccc.base.UNSPECIFIED;
-var ELLIPSIS = Sym('...');
-
 function C(contents) {
-  return new ccc.syntax.Capture(contents);
+  if (contents instanceof Array)
+    return new ccc.syntax.Capture(contents);
+  return new ccc.syntax.Capture(ccc.base.build(contents));
 }
 
-function T(templateForm, captures, opt_expectedOutput) {
-  var template = new ccc.syntax.Template(templateForm);
+function T(templateSpec, captures, opt_expectedOutputSpec) {
+  var template = new ccc.syntax.Template(ccc.base.build(templateSpec));
   var output = template.expand(captures);
-  if (goog.isDef(opt_expectedOutput) && !output.equal(opt_expectedOutput))
-    fail('Template expansion mismatch.\n' +
-         'Expected: ' + opt_expectedOutput.toString() +
-         '\nActual: ' + output.toString() + '\n');
+  if (goog.isDef(opt_expectedOutputSpec)) {
+    var expectedOutput = ccc.base.build(opt_expectedOutputSpec);
+    if (!output.equal(expectedOutput))
+      fail('Template expansion mismatch.\n' +
+           'Expected: ' + expectedOutput.toString() +
+           '\nActual: ' + output.toString() + '\n');
+  }
 }
 
-function F(templateForm, captures) {
-  var template = new ccc.syntax.Template(templateForm);
+function F(templateSpec, captures) {
+  var template = new ccc.syntax.Template(ccc.base.build(templateSpec));
   try {
     var output = template.expand(captures);
   } catch (e) {
@@ -43,226 +37,223 @@ function F(templateForm, captures) {
 }
 
 function testSimpleTemplates() {
-  T(TRUE, {}, TRUE);
-  T(List([TRUE, FALSE, Str("Hello, world!")]), {},
-    List([TRUE, FALSE, Str("Hello, world!")]));
-  T(NIL, {}, NIL);
-  T(Num(42), {}, Num(42));
-  T(Vec([Num(1), Num(2), Num(3)]), {}, Vec([Num(1), Num(2), Num(3)]));
+  T(true, {}, true);
+  T([true, false, { 'str': "Hello, world!" }],
+    {},
+    [true, false, { 'str': "Hello, world!" }]);
+  T(null, {}, null);
+  T(42, {}, 42);
+  T({ 'vec': [1, 2, 3] }, {}, { 'vec': [1, 2, 3] });
 }
 
 function testRank0Expansion() {
-  // (a)
-  T(Sym('a'),
-    {
-      'a': C(Num(42))
-    },
-    Num(42));
+  // a
+  T('a', { 'a': C(42) }, 42);
+
   // (a b)
-  T(List([Sym('a'), Sym('b')]),
+  T(['a', 'b'],
     {
-      'a': C(Num(42))
+      'a': C(42)
     },
-    List([Num(42), Sym('b')]));
+    [42, 'b']);
+
   // (a . b)
-  T(List([Sym('a')], Sym('b')),
+  T({ 'list': ['a'], 'tail': 'b' },
     {
-      'a': C(List([Num(1), Num(2)])),
-      'b': C(List([Num(3), Num(4)]))
+      'a': C({ 'list': [1, 2] }),
+      'b': C({ 'list': [3, 4] })
     },
-    List([List([Num(1), Num(2)]), Num(3), Num(4)]));
+    [[1, 2], 3, 4]);
 }
 
 function testRank1Expansion() {
   // (a ...)
-  T(List([Sym('a'), ELLIPSIS]),
+  T(['a', '...'],
     {
-      'a': C([C(Num(1)), C(Num(2)), C(Num(3))])
+      'a': C([C(1), C(2), C(3)])
     },
-    List([Num(1), Num(2), Num(3)]));
+    [1, 2, 3]);
+
   // (a ... b ...)
-  T(List([Sym('a'), ELLIPSIS, Sym('b'), ELLIPSIS]),
+  T(['a', '...', 'b', '...'],
     {
-      'a': C([C(Num(1)), C(Num(2))]),
-      'b': C([C(Sym('a')), C(Sym('b')), C(Sym('c'))])
+      'a': C([C(1), C(2)]),
+      'b': C([C('a'), C('b'), C('c')])
     },
-    List([Num(1), Num(2), Sym('a'), Sym('b'), Sym('c')]));
+    [1, 2, 'a', 'b', 'c']);
+
   // #(#t a ...)
-  T(Vec([TRUE, Sym('a'), ELLIPSIS]),
+  T({ 'vec': [true, 'a', '...'] },
     {
-      'a': C([C(Num(1)), C(Num(2))])
+      'a': C([C(1), C(2)])
     },
-    Vec([TRUE, Num(1), Num(2)]));
+    { 'vec': [true, 1, 2] });
+
   // (a ...) with empty capture
-  T(List([Sym('a'), ELLIPSIS]),
+  T(['a', '...'],
     {
       'a': C([])
     },
-    ccc.base.NIL);
+    []);
+
   // #(a ...) with empty capture
-  T(Vec([Sym('a'), ELLIPSIS]),
+  T({ 'vec': ['a', '...'] },
     {
       'a': C([])
     },
-    Vec([]));
+    { 'vec': [] });
+
   // (1 (a ...) 2 b ... c ...) with empty |c|
-  T(List([Num(1), List([Sym('a'), ELLIPSIS]), Num(2), Sym('b'), ELLIPSIS]),
+  T([1, ['a', '...'], 2, 'b', '...', 'c', '...'],
     {
-      'a': C([C(Num(42)), C(Num(43))]),
-      'b': C([C(Num(44)), C(Num(45))]),
+      'a': C([C(42), C(43)]),
+      'b': C([C(44), C(45)]),
       'c': C([])
     },
-    List([Num(1), List([Num(42), Num(43)]), Num(2), Num(44), Num(45)]));
+    [1, [42, 43], 2, 44, 45]);
 }
 
 function testExpansionLimiting() {
   // ((a b) ...) with equal capture lengths
-  T(List([List([Sym('a'), Sym('b')]), ELLIPSIS]),
+  T([['a', 'b'], '...'],
     {
-      'a': C([C(Num(1)), C(Num(2))]),
-      'b': C([C(Num(3)), C(Num(4))])
+      'a': C([C(1), C(2)]),
+      'b': C([C(3), C(4)])
     },
-    List([List([Num(1), Num(3)]), List([Num(2), Num(4)])]));
+    [[1, 3], [2, 4]]);
+
   // ((a b) ...) with longer |a| capture
-  T(List([List([Sym('a'), Sym('b')]), ELLIPSIS]),
+  T([['a', 'b'], '...'],
     {
-      'a': C([C(Num(1)), C(Num(2)), C(Num(42))]),
-      'b': C([C(Num(3)), C(Num(4))])
+      'a': C([C(1), C(2), C(42)]),
+      'b': C([C(3), C(4)])
     },
-    List([List([Num(1), Num(3)]), List([Num(2), Num(4)])]));
+    [[1, 3], [2, 4]]);
+
   // ((a b) ...) with longer |b| capture
-  T(List([List([Sym('a'), Sym('b')]), ELLIPSIS]),
+  T([['a', 'b'], '...'],
     {
-      'a': C([C(Num(1)), C(Num(2))]),
-      'b': C([C(Num(3)), C(Num(4)), C(Num(42))])
+      'a': C([C(1), C(2)]),
+      'b': C([C(3), C(4), C(42)])
     },
-    List([List([Num(1), Num(3)]), List([Num(2), Num(4)])]));
+    [[1, 3], [2, 4]]);
+
   // ((a b) ...) with empty |b| capture
-  T(List([List([Sym('a'), Sym('b')]), ELLIPSIS]),
+  T([['a', 'b'], '...'],
     {
-      'a': C([C(Num(1)), C(Num(2)), C(Num(3))]),
+      'a': C([C(1), C(2), C(3)]),
       'b': C([])
     },
-    ccc.base.NIL);
+    []);
+
   // ((a b ...) ...) with empty a and b captures
-  T(List([List([Sym('a'), Sym('b'), ELLIPSIS]), ELLIPSIS]),
+  T([['a', 'b', '...'], '...'],
     {
       'a': C([]),
       'b': C([C([])])
     },
-    ccc.base.NIL);
+    []);
 }
 
 function testInvalidExpansionRank() {
   // (a ...) with terminal capture in |a|
-  F(List([Sym('a'), ELLIPSIS]),
+  F(['a', '...'],
     {
-      'a': C(Num(42))
+      'a': C(42)
     });
+
   // (a) with non-terminal capture in |a|
-  F(List([Sym('a')]),
+  F(['a'],
     {
-      'a': C([C(Num(1)), C(Num(2))])
+      'a': C([C(1), C(2)])
     });
 }
 
 function testMultipleExpansionsOfOneVariable() {
   // (a a)
-  T(List([Sym('a'), Sym('a')]),
+  T(['a', 'a'],
     {
-      'a': C(Num(42))
+      'a': C(42)
     },
-    List([Num(42), Num(42)]));
+    [42, 42]);
+
   // (a ... a ...)
-  T(List([Sym('a'), ELLIPSIS, Sym('a'), ELLIPSIS]),
+  T(['a', '...', 'a', '...'],
     {
-      'a': C([C(Num(0)), C(Num(1))])
+      'a': C([C(0), C(1)])
     },
-    List([Num(0), Num(1), Num(0), Num(1)]));
+    [0, 1, 0, 1]);
+
   // ((a b) ...) with terminal capture in |a|
-  T(List([List([Sym('a'), Sym('b')]), ELLIPSIS]),
+  T([['a', 'b'], '...'],
     {
-      'a': C(Num(42)),
-      'b': C([C(Sym('foo')), C(Sym('bar'))])
+      'a': C(42),
+      'b': C([C('foo'), C('bar')])
     },
-    List([List([Num(42), Sym('foo')]), List([Num(42), Sym('bar')])]));
+    [[42, 'foo'], [42, 'bar']]);
 }
 
 function testRank2Expansion() {
   // ((a ... b ...) ...)
-  T(List([List([Sym('a'), ELLIPSIS, Sym('b'), ELLIPSIS]), ELLIPSIS]),
+  T([['a', '...', 'b', '...'], '...'],
     {
       // a <- [[1, 3, 5], [a, c, e]]
-      'a': C([C([C(Num(1)), C(Num(3)), C(Num(5))]),
-              C([C(Sym('a')), C(Sym('c')), C(Sym('e'))])]),
+      'a': C([C([C(1), C(3), C(5)]),
+              C([C('a'), C('c'), C('e')])]),
       // b <- [[2, 4, 6], [b, d, f]].
-      'b': C([C([C(Num(2)), C(Num(4)), C(Num(6))]),
-              C([C(Sym('b')), C(Sym('d')), C(Sym('f'))])])
+      'b': C([C([C(2), C(4), C(6)]),
+              C([C('b'), C('d'), C('f')])])
     },
-    // ((1 3 5 2 4 6) (a c e b d f))
-    List([List([Num(1), Num(3), Num(5), Num(2), Num(4), Num(6)]),
-          List([Sym('a'), Sym('c'), Sym('e'), Sym('b'), Sym('d'), Sym('f')])]));
+    [[1, 3, 5, 2, 4, 6], ['a', 'c', 'e', 'b', 'd', 'f']]);
 
   // ((a ... a ...) ...)
-  T(List([List([Sym('a'), ELLIPSIS, Sym('a'), ELLIPSIS]), ELLIPSIS]),
+  T([['a', '...', 'a', '...'], '...'],
     {
       // a <- [[1, 3, 5], [a, c, e]]
-      'a': C([C([C(Num(1)), C(Num(3)), C(Num(5))]),
-              C([C(Sym('a')), C(Sym('c')), C(Sym('e'))])]),
+      'a': C([C([C(1), C(3), C(5)]),
+              C([C('a'), C('c'), C('e')])]),
     },
-    // ((1 3 5 1 3 5) (a c e a c e))
-    List([List([Num(1), Num(3), Num(5), Num(1), Num(3), Num(5)]),
-          List([Sym('a'), Sym('c'), Sym('e'), Sym('a'), Sym('c'), Sym('e')])]));
+    [[1, 3, 5, 1, 3, 5], ['a', 'c', 'e', 'a', 'c', 'e']]);
 }
 
 function testHigherRankExpansions() {
   // (((c ... b) ... a) ...)
-  T(List([
-      List([List([Sym('c'), ELLIPSIS, Sym('b')]),
-            ELLIPSIS, Sym('a')]),
-      ELLIPSIS]),
+  T([[['c', '...', 'b'], '...', 'a'], '...'],
     {
       // a <- [1, a, !]
-      'a': C([C(Num(1)), C(Sym('a')), C(Sym('!'))]),
+      'a': C([C(1), C('a'), C('!')]),
       // b <- [[2, 6], [b, f], [$, $]]
-      'b': C([C([C(Num(2)), C(Num(6))]),
-              C([C(Sym('b')), C(Sym('f'))]),
-              C([C(Sym('$'))])]),
+      'b': C([C([C(2), C(6)]),
+              C([C('b'), C('f')]),
+              C([C('$')])]),
       // c <- [[[3, 4, 5], [7, 8]], [[c, d, e], [g]], [[$, $]]]
-      'c': C([C([C([C(Num(3)), C(Num(4)), C(Num(5))]),
-                 C([C(Num(7)), C(Num(8))])]),
-              C([C([C(Sym('c')), C(Sym('d')), C(Sym('e'))]),
-                 C([C(Sym('g'))])]),
-              C([C([C(Sym('$')), C(Sym('$'))])])])
+      'c': C([C([C([C(3), C(4), C(5)]),
+                 C([C(7), C(8)])]),
+              C([C([C('c'), C('d'), C('e')]),
+                 C([C('g')])]),
+              C([C([C('$'), C('$')])])])
     },
     // (((3 4 5 2) (7 8 6) 1) ((c d e b) (g f) a) (($ $ $) !))
-    List([List([List([Num(3), Num(4), Num(5), Num(2)]),
-                List([Num(7), Num(8), Num(6)]), Num(1)]),
-          List([List([Sym('c'), Sym('d'), Sym('e'), Sym('b')]),
-                List([Sym('g'), Sym('f')]), Sym('a')]),
-          List([List([Sym('$'), Sym('$'), Sym('$')]), Sym('!')])]));
+    [[[3, 4, 5, 2], [7, 8, 6], 1], [['c', 'd', 'e', 'b'], ['g', 'f'], 'a'],
+     [['$', '$', '$'], '!']]);
 
   // (((a b ...) ...) ...)
-  T(List([List([List([Sym('a'), Sym('b'), ELLIPSIS]), ELLIPSIS]), ELLIPSIS]),
+  T([[['a', 'b', '...'], '...'], '...'],
     {
       // a <- [[1, 2, 3, 4, 5], [5, 4, 3, 2, 1]]
-      'a': C([C([C(Num(1)), C(Num(2)), C(Num(3)), C(Num(4)), C(Num(5))]),
-              C([C(Num(5)), C(Num(4)), C(Num(3)), C(Num(2)), C(Num(1))])]),
+      'a': C([C([C(1), C(2), C(3), C(4), C(5)]),
+              C([C(5), C(4), C(3), C(2), C(1)])]),
       // b <- [[[a, b, c, d], [x, y, z], [7, 7, 7, 7, 7]],
       //       [[d, c, b, a], [z, y, x], [6, 6, 6, 6]]]
-      'b': C([C([C([C(Sym('a')), C(Sym('b')), C(Sym('c')), C(Sym('d'))]),
-                 C([C(Sym('x')), C(Sym('y')), C(Sym('z'))]),
-                 C([C(Num(7)), C(Num(7)), C(Num(7)), C(Num(7)), C(Num(7))])]),
-              C([C([C(Sym('d')), C(Sym('c')), C(Sym('b')), C(Sym('a'))]),
-                 C([C(Sym('z')), C(Sym('y')), C(Sym('x'))]),
-                 C([C(Num(6)), C(Num(6)), C(Num(6)), C(Num(6))])])])
+      'b': C([C([C([C('a'), C('b'), C('c'), C('d')]),
+                 C([C('x'), C('y'), C('z')]),
+                 C([C(7), C(7), C(7), C(7), C(7)])]),
+              C([C([C('d'), C('c'), C('b'), C('a')]),
+                 C([C('z'), C('y'), C('x')]),
+                 C([C(6), C(6), C(6), C(6)])])])
     },
     // (((1 a b c d) (2 x y z) (3 7 7 7 7 7))
     //  ((5 d c b a) (4 z y x) (3 6 6 6 6)))
-    List([List([List([Num(1), Sym('a'), Sym('b'), Sym('c'), Sym('d')]),
-                List([Num(2), Sym('x'), Sym('y'), Sym('z')]),
-                List([Num(3), Num(7), Num(7), Num(7), Num(7), Num(7)])]),
-          List([List([Num(5), Sym('d'), Sym('c'), Sym('b'), Sym('a')]),
-                List([Num(4), Sym('z'), Sym('y'), Sym('x')]),
-                List([Num(3), Num(6), Num(6), Num(6), Num(6)])])]));
+    [[[1, 'a', 'b', 'c', 'd'], [2, 'x', 'y', 'z'], [3, 7, 7, 7, 7, 7]],
+     [[5, 'd', 'c', 'b', 'a'], [4, 'z', 'y', 'x'], [3, 6, 6, 6, 6]]]);
 }

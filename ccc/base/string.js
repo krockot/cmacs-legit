3ca47d205c85a623e306d0c9c4e007bb999f04fa
Ken Rockot <ken@oz.gs>
2014-11-10 19:02:23 -0800

    Add string-length and make-string

--- ccc/base/string.js
+++ ccc/base/string.js
@@ -11,4 +11,20 @@ ccc.baseUtil.registerProcedures(ccc.base, {
     args: [null],
     impl: ccc.isString
   },
+
+  'make-string': {
+    args: [ccc.isInteger],
+    optionalArgs: [ccc.isChar],
+    impl: function(k, opt_chr) {
+      var chr = (goog.isDef(opt_chr)
+          ? String.fromCharCode(opt_chr.value()) :
+          '\0');
+      return Array(k + 1).join(chr);
+    }
+  },
+
+  'string-length': {
+    args: [ccc.isString],
+    impl: function(x) { return x.length; }
+  },
 });
--- ccc/base/string_test.js
+++ ccc/base/string_test.js
@@ -23,3 +23,26 @@ function testStringPredicate() {
     F(['string?', new String('hello'), new String('goodbye')]),
   ]);
 }
+
+function testMakeString() {
+  RunTests([
+    T(['make-string', 0], new String('')),
+    T(['make-string', 1], new String('\0')),
+    T(['make-string', 3, new ccc.Char(955)], new String('\u03bb\u03bb\u03bb')),
+    F(['make-string']),
+    F(['make-string', new ccc.Char(0)]),
+    F(['make-string', true]),
+    F(['make-string', 1, new ccc.Char(0), 2]),
+  ]);
+}
+
+function testStringLength() {
+  RunTests([
+    T(['string-length', new String('')], 0),
+    T(['string-length', new String('hello')], 5),
+    F(['string-length']),
+    F(['string-length', new String(''), new String('')]),
+    F(['string-length', 1]),
+    F(['string-length', 'string-length']),
+  ]);
+}

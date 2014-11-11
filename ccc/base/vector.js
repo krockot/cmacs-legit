f21bbb29359047c79d6d42373b935bfe71928a71
Ken Rockot <ken@oz.gs>
2014-11-10 22:11:09 -0800

    Add vector and make-vector

--- ccc/base/vector.js
+++ ccc/base/vector.js
@@ -12,4 +12,20 @@ ccc.base.registerProcedures({
     args: [null],
     impl: ccc.isVector
   },
+
+  'vector': {
+    optionalArgs: null,
+    impl: function() {
+      return new ccc.Vector(goog.array.toArray(arguments));
+    }
+  },
+
+  'make-vector': {
+    args: [ccc.isInteger],
+    optionalArgs: [null],
+    impl: function(k, opt_data) {
+      var data = goog.isDef(opt_data) ? opt_data : ccc.UNSPECIFIED;
+      return new ccc.Vector(goog.array.repeat(data, k));
+    }
+  },
 });
--- ccc/base/vector_test.js
+++ ccc/base/vector_test.js
@@ -24,3 +24,25 @@ function testVectorPredicate() {
     F(['vector?', V([]), V([])]),
   ]);
 }
+
+function testVector() {
+  RunTests([
+    T(['vector'], V([])),
+    T(['vector', 1, 2, 3], V([1, 2, 3])),
+    T(['vector', []], V([[]])),
+    T(['vector', true, false], V([true, false])),
+  ]);
+}
+
+function testMakeVector() {
+  RunTests([
+    T(['make-vector', 0], V([])),
+    T(['make-vector', 1], V([ccc.UNSPECIFIED])),
+    T(['make-vector', 2], V([ccc.UNSPECIFIED, ccc.UNSPECIFIED])),
+    T(['make-vector', 3, 6], V([6, 6, 6])),
+    T(['make-vector', 4, true], V([true, true, true, true])),
+    F(['make-vector']),
+    F(['make-vector', false]),
+    F(['make-vector', 1, 1, 1]),
+  ]);
+}

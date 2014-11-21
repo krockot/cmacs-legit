8dc04f3c4d570fa7ce6ff7bbe1ecf957c6134fe8
Ken Rockot <ken@oz.gs>
2014-11-20 23:03:34 -0800

    More prelude tests: cond, when, unless

--- ccc/base/prelude_test.js
+++ ccc/base/prelude_test.js
@@ -11,25 +11,20 @@ function setUpPage() {
   setUpBaseTest('ccc.base.PreludeTest');
 }
 
-function testBegin() {
+function makeCountedCaller() {
   var count = 0;
-  var native1 = new ccc.NativeProcedure(
-      function(environment, args, continuation) {
-    assertEquals(0, count++);
-    return continuation(1);
-  });
-  var native2 = new ccc.NativeProcedure(
-      function(environment, args, continuation) {
-    assertEquals(1, count++);
-    return continuation(2);
-  });
-  var native3 = new ccc.NativeProcedure(
-      function(environment, args, continuation) {
-    assertEquals(2, count);
-    return continuation(3);
-  });
+  return function(expectedCount) {
+    return new ccc.NativeProcedure(function(e, a, c) {
+      assertEquals(expectedCount, ++count);
+      return c(count);
+    })
+  };
+}
+
+function testBegin() {
+  var called = makeCountedCaller();
   RunTests([
-    T(['begin', [native1], [native2], [native3]], 3),
+    T(['begin', [called(1)], [called(2)], [called(3)]], 3),
   ]);
 }
 
@@ -82,5 +77,36 @@ function testLetRec() {
                     'n',
                     ['f', ['-', 'x', 1], ['+', 'n', 'x']]]]]],
         ['f', 10, 0]], 55 ),
+  ]);
+}
+
+function testCond() {
+  var called = makeCountedCaller();
+  RunTests([
+    T(['cond'], ccc.UNSPECIFIED),
+    T(['cond', [false, 42], [false, 43]], ccc.UNSPECIFIED),
+    T(['cond', [true, 1, 2, 3, 4]], 4),
+    T(['cond', [false, 1, 2, 3], [true, 1, 2]], 2),
+    T(['cond', [true, [called(1)], [called(2)], [called(3)]]], 3),
+    F(['cond', [true]]),
+    F(['cond', 1]),
+    F(['cond', 1, 2]),
+    F(['cond', [false, 1, 2, 3], 1]),
+  ]);
+}
+
+function testWhen() {
+  var called = makeCountedCaller();
+  RunTests([
+    T(['when', true, [called(1)], [called(2)]], 2),
+    T(['when', false, 'never-evaluated'], ccc.UNSPECIFIED),
+  ]);
+}
+
+function testUnless() {
+  var called = makeCountedCaller();
+  RunTests([
+    T(['unless', false, [called(1)], [called(2)]], 2),
+    T(['unless', true, 'never-evaluated'], ccc.UNSPECIFIED),
   ]);
 }
